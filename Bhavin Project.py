d092a712d875236d0308acdd9852ef76bf8375e4
Rockydedhia <55575648+Rockydedhia@users.noreply.github.com>
2019-12-10 17:51:30 +0530

    Add files via upload

---
+++ Bhavin Project.py
@@ -0,0 +1,30 @@
+import re
+import xlsxwriter
+import urllib.request
+url = input('Enter a URl: ')
+urllib.request.urlretrieve(url, 'bhavin.txt')
+
+for line in open('bhavin.txt'):
+    re.sub(
+        '[@|<|.|>|.|!|.|:|.|;|.|{|.|}|.|</|.|"|.|-|.|#|.|(|.|)|.|,|.|"|.|=|.]', '', line)
+
+print(line.readline())
+
+ search_word = input("Enter a Keyword: ")
+  count = 0
+   if search_word in contents:
+        ct = contents.count(search_word)
+        book = xlsxwriter.Workbook('web.xlsx')
+        sheet = book.add_worksheet()
+        sheet.write('A1', 'Keywords')
+        sheet.write('B1', 'Occur')
+        sheet.write('A2', search_word)
+        sheet.write('B2', ct)
+        book.close()
+        print('Your Record Save Succesfuly!!!')
+
+    else:
+        print('word not found')
+##
+##
+##
